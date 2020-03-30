0eef4d132b3691d6f5a4bdf9c9ed082434a3135b
13266708958 <1932055255@qq.com>
2020-03-30 22:35:08 +0800

    0330.22.35

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,22 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    students: [
+      { name: 'ji', age: '20', sex: 'm' },
+      { name: 'he', age: '21', sex: 'w' }
+    ],
+    count: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  HandleAdd() {
+    this.setData({
+      count: this.data.count += 1
+    }),
+      console.log(this.data.count)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  HandleSub() {
+    this.setData({
+      count: this.data.count -= 1
+    })
   }
+
 })
